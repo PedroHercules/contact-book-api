c99a93691408c7053852ff3fd224a01b80d6b9ff
PedroHercules <pedroherculesdantas@gmail.com>
2023-02-20 19:26:06 -0300

    feat: cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.APP_PORT || 3333);
