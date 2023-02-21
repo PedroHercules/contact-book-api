53c63261274ff52b594df729aba03235ec64973d
PedroHercules <pedroherculesdantas@gmail.com>
2023-02-21 20:52:57 -0300

    feat:  added main route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,10 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      api: 'Contact book API',
+      version: '1.0.0',
+    };
   }
 }
